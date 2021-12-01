9e5159bcacdb0e4634c52032b30321f80f34e4c4
Lucas <lucasgunner3@gmail.com>
2021-11-30 21:30:17 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2Attributes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ex2Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetAssembly(typeof(object));
+            var obsoleteClasses = assembly?.GetTypes().Where(x => x.GetCustomAttributes<ObsoleteAttribute>().Any());
+
+            foreach (var obsoleteClass in obsoleteClasses)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine($"Class: {obsoleteClass.Name}");
+                
+                Console.ForegroundColor = default;
+
+                foreach (var method in obsoleteClass.GetMethods())
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{obsoleteClass.FullName}.{method.Name}()");
+                }
+            }
+        }
+    }
+}
